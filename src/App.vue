078630fc2254d8efc553a55cdf209d7ebcdbb56c
Ryan Dsilva <ryan.dsilva.98@gmail.com>
2022-12-04 00:31:41 -0500

    root page ui

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,25 @@ import { RouterView } from 'vue-router'
 
 <template>
   <v-app>
-    <v-app-bar title="IndyBMS" absolute color="primary">
-      <template v-slot:prepend>
-        <v-btn icon="fa:fas fa-ticket" to="/"></v-btn>
-      </template>
+    <v-app-bar absolute color="indigo" density="comfortable">
+      <v-btn variant="plain" to="/" color="white">
+        <v-app-bar-title>IndyBMS</v-app-bar-title>
+      </v-btn>
       <template v-slot:append>
-        <v-btn icon="fa:fas fa-search"></v-btn>
-        <v-btn color="white" variant="text" class="mx-2" rounded="xl" size="large" to="/login">Login</v-btn>
-        <v-btn color="white" variant="text" class="mx-2" rounded="xl" size="large" to="/register">Sign Up</v-btn>
+        <v-btn color="white" variant="plain" class="mx-4" size="medium" to="/login">Login</v-btn>
+        <v-btn color="white" variant="plain" class="mx-2 ml-4" size="medium" to="/register">Register</v-btn>
       </template>
     </v-app-bar>
     <v-main>
       <RouterView />
     </v-main>
-    <v-footer absolute app class="bg-primary">
+    <v-footer absolute app class="bg-indigo">
       <v-row justify="center" no-gutters>
-        <v-btn v-for="link in links" :key="link" :to="link.path" color="white" variant="text" class="mx-2" rounded="xl">
+        <v-btn v-for="link in links" :key="link" color="white" variant="text" class="mx-4 btn-text">
           {{ link.title }}
         </v-btn>
-        <v-col class="text-center mt-4" cols="12">
-          &copy; {{ new Date().getFullYear() }} — <strong>IndyBMS</strong>
+        <v-col class="text-center mt-0" cols="12">
+          &copy; {{ new Date().getFullYear() }} — <strong> &copy; IndyBMS</strong>
         </v-col>
       </v-row>
     </v-footer>
@@ -47,3 +46,9 @@ export default {
   }),
 }
 </script>
+
+<style scoped>
+.btn-text {
+  font-size: 0.8rem;
+}
+</style>
